3f3d5a73111185bfbfef822f47a21daf2f5efca7
ion <ionix@protonmail.com>
2016-12-31 18:01:28 +0100

    Add project files.

---
+++ dnpatch.script.patcher/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dnpatch.script.patcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
